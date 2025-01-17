43fe5c21e750e6a36d47f12eb7143263223b6aa0
Auh3b <ngaiyayec@yahoo.com>
2025-01-17 13:19:55 +0200

    added feature delete button

--- src/components/UI/DrawTools/index.jsx
+++ src/components/UI/DrawTools/index.jsx
@@ -1,16 +1,32 @@
-import { Box, Paper, Tab, Tabs } from "@mui/material";
-import React, { useState } from "react";
+import {
+  Box,
+  Divider,
+  Grid2,
+  IconButton,
+  Paper,
+  Tab,
+  Tabs,
+} from "@mui/material";
+import React from "react";
 import CropSquareOutlinedIcon from "@mui/icons-material/CropSquareOutlined";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined";
-import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import PanToolOutlinedIcon from "@mui/icons-material/PanToolOutlined";
+import PentagonOutlinedIcon from "@mui/icons-material/PentagonOutlined";
 import { DRAW_MODES } from "../../../utils/drawingUtils";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+import { useDispatch } from "react-redux";
+import { setDrawingProps } from "../../../store/mapStore";
 
 export default function index({ mode, onStart, onStop }) {
+  const dispatch = useDispatch();
   const handleChange = (_e, value) => {
     if (value === DRAW_MODES.FREE) return onStop();
     onStart(value);
+  };
+
+  const handleFeatureDelete = () => {
+    dispatch(setDrawingProps(null));
   };
 
   return (
@@ -21,36 +37,46 @@ export default function index({ mode, onStart, onStop }) {
         left: (theme) => theme.spacing(2),
       }}>
       <Paper>
-        <Tabs
-          value={mode}
-          onChange={handleChange}
-          TabIndicatorProps={{
-            sx: {
-              display: "none",
-            },
-          }}
-          orientation="vertical">
-          <CustomTab
-            icon={<PanToolOutlinedIcon fontSize="small" />}
-            value={DRAW_MODES.FREE}
-          />
-          <CustomTab
-            icon={<PlaceOutlinedIcon fontSize="small" />}
-            value={DRAW_MODES.POINT}
-          />
-          <CustomTab
-            icon={<CircleOutlinedIcon fontSize="small" />}
-            value={DRAW_MODES.CIRCLE}
-          />
-          <CustomTab
-            icon={<CropSquareOutlinedIcon fontSize="small" />}
-            value={DRAW_MODES.RECTANGLE}
-          />
-          <CustomTab
+        <Grid2 container direction={"column"} alignItems={"center"}>
+          <Tabs
+            value={mode}
+            onChange={handleChange}
+            TabIndicatorProps={{
+              sx: {
+                display: "none",
+              },
+            }}
+            orientation="vertical">
+            <CustomTab
+              icon={<PanToolOutlinedIcon fontSize="small" />}
+              value={DRAW_MODES.FREE}
+            />
+            <CustomTab
+              icon={<PlaceOutlinedIcon fontSize="small" />}
+              value={DRAW_MODES.POINT}
+            />
+            <CustomTab
+              icon={<CircleOutlinedIcon fontSize="small" />}
+              value={DRAW_MODES.CIRCLE}
+            />
+            <CustomTab
+              icon={<CropSquareOutlinedIcon fontSize="small" />}
+              value={DRAW_MODES.RECTANGLE}
+            />
+            <CustomTab
+              icon={<PentagonOutlinedIcon fontSize="small" />}
+              value={DRAW_MODES.POLYGON}
+            />
+            {/* <CustomTab
             icon={<TimelineOutlinedIcon fontSize="small" />}
             value={DRAW_MODES.LINE}
-          />
-        </Tabs>
+          /> */}
+          </Tabs>
+          <Divider orientation="horizontal" flexItem />
+          <IconButton onClick={handleFeatureDelete}>
+            <DeleteOutlinedIcon fontSize="small" />
+          </IconButton>
+        </Grid2>
       </Paper>
     </Box>
   );
