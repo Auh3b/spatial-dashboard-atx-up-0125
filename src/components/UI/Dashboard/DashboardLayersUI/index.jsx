417ae505402360e21f0e87bbb6084c9ee268889c
Auh3b <ngaiyayec@yahoo.com>
2025-03-17 18:40:55 +0200

    changed layout

--- src/components/UI/Dashboard/DashboardLayersUI/index.jsx
+++ src/components/UI/Dashboard/DashboardLayersUI/index.jsx
@@ -1,14 +1,43 @@
 import { Box, Button, Divider } from "@mui/material";
-import React, { Fragment, useMemo, useState } from "react";
-import { useSelector } from "react-redux";
-import { getLayers, getSelectedLayer } from "../../../../store/mapStore";
+import React, { Fragment, useCallback, useMemo, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getLayers,
+  getSelectedLayer,
+  removeSelectedLayer,
+  setSelectedLayer,
+} from "../../../../store/mapStore";
 import LayerUI from "./LayerUI";
 import AddLayerModal from "./components/AddLayerModal";
 
 export default function DashboardLayersUI({ selected, index }) {
   const _layers = useSelector((state) => getLayers(state));
-  const layers = useMemo(() => Object.values(_layers), [_layers]);
+  const layers = useMemo(
+    () => Object.values(_layers).filter((d) => !d?.system),
+    [_layers],
+  );
   const selectedLayer = useSelector((state) => getSelectedLayer(state));
+  const dispatch = useDispatch();
+
+  const handleClick = useCallback(
+    (id) => {
+      return () => {
+        if (selectedLayer !== id) {
+          dispatch(setSelectedLayer(id));
+          return;
+        }
+        dispatch(removeSelectedLayer());
+        // dispatch(removeFilteredData());
+        // dispatch(setDrawingProps(null));
+      };
+    },
+    [selectedLayer],
+  );
+
+  const handleDelete = (id) => {
+    return () => dispatch(removeLayer({ id }));
+  };
+
   return (
     <Fragment>
       {selected === index && (
@@ -17,7 +46,13 @@ export default function DashboardLayersUI({ selected, index }) {
           <Divider />
           {Boolean(layers.length) &&
             layers.map((d) => (
-              <LayerUI key={d.id} {...d} selected={selectedLayer} />
+              <LayerUI
+                key={d.id}
+                {...d}
+                selected={selectedLayer}
+                onDelete={handleDelete(d.id)}
+                onClick={handleClick(d.id)}
+              />
             ))}
         </Box>
       )}
