ef516ab1d852fb78a2ac0a8733ccf68eacf19042
Auh3b <ngaiyayec@yahoo.com>
2025-03-17 18:41:52 +0200

    initiate draw layer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,21 @@
 import { Grid2 } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { drawingConfig } from "../data/layerConfig";
+import { addLayer, removeLayer } from "../store/mapStore";
 import Dashboard from "./UI/Dashboard";
 import Header from "./UI/Header";
 
+const drawLayer = drawingConfig;
+
 export default function Layout() {
   const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(addLayer({ id: drawLayer.id, value: drawLayer }));
+    return () => {
+      dispatch(removeLayer({ id: drawLayer.id }));
+    };
+  });
   return (
     <Grid2
       container
